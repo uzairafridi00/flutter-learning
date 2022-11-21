54c0bbb153ce0a6886160d47fe8588c039e427d9
Uzair Afridi <65648689+uzairafridi00@users.noreply.github.com>
2022-11-21 08:01:03 +0500

    List View Builder applied for Transactions

--- Flutter Projects/expense_tracker/lib/widgets/transactions_list.dart
+++ Flutter Projects/expense_tracker/lib/widgets/transactions_list.dart
@@ -10,46 +10,51 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tx) {
-        return Card(
-            elevation: 5,
-            child: Row(
-              children: [
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                    color: Colors.purple,
-                    width: 2,
-                  )),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    'Rs ' + tx.amount.toString(),
-                    // '\$${tx.amount}',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(tx.title,
-                        style: TextStyle(
+    return Container(
+      height: 450,
+      width: 340,
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+              elevation: 5,
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    )),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Rs ' + transactions[index].amount.toString(),
+                      // '\$${tx.amount}',
+                      style: TextStyle(
                           fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        )),
-                    Text(
-                      DateFormat.yMMMd().format(tx.date),
-                      style: TextStyle(fontSize: 14, color: Colors.grey),
-                    )
-                  ],
-                ),
-              ],
-            ));
-      }).toList(),
+                          fontSize: 20,
+                          color: Colors.purple),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(transactions[index].title,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          )),
+                      Text(
+                        DateFormat.yMMMd().format(transactions[index].date),
+                        style: TextStyle(fontSize: 14, color: Colors.grey),
+                      )
+                    ],
+                  ),
+                ],
+              ));
+        },
+        itemCount: transactions.length,
+      ),
     );
   }
 }
