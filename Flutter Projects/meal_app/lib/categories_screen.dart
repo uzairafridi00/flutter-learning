e037de90062321d4aac252f1e85d4ecb8e568d66
Uzair Afridi <65648689+uzairafridi00@users.noreply.github.com>
2022-12-14 08:45:46 +0500

    One Screen added with custom theme and font styles

--- Flutter Projects/meal_app/lib/categories_screen.dart
+++ Flutter Projects/meal_app/lib/categories_screen.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 
+import './dummy_data.dart';
+import 'category_item.dart';
+
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: [],
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 2 / 3,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 1,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map((catData) =>
+                CategoryItem(title: catData.title, color: catData.color))
+            .toList(),
       ),
     );
   }
--- Flutter Projects/meal_app/lib/main.dart
+++ Flutter Projects/meal_app/lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+
+import 'categories_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,29 +17,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
+            .copyWith(secondary: Colors.amber),
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyLarge: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyMedium: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              titleMedium: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
       ),
-      home: const MyHomePage(title: 'DeliMeals'),
+      home: CategoriesScreen(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: const Center(child: Text("Navigation Time")));
   }
 }
