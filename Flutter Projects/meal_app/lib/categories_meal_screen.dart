5f6a93546b9f77d4c42b814c813a0be55dba0661
Uzair Afridi <65648689+uzairafridi00@users.noreply.github.com>
2022-12-17 12:00:29 +0500

    Modal and Enums added

--- Flutter Projects/meal_app/lib/categories_meal_screen.dart
+++ Flutter Projects/meal_app/lib/categories_meal_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './dummy_data.dart';
+
 class CategoriesMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
   // final String categoryId;
@@ -13,11 +15,18 @@ class CategoriesMealsScreen extends StatelessWidget {
     final routeArgs = ModalRoute.of(context)?.settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle!),
       ),
-      body: const Center(child: Text('The Recipes for the Category!!')),
+      body: ListView.builder(itemBuilder: (ctx, index) {
+        return Text(categoryMeals[index].title);
+      }, itemCount: categoryMeals.length,),
     );
   }
 }
