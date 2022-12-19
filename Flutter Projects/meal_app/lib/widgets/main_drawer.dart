5a649544e5ae26dfc0b8997815e1ca5011be37ed
Uzair Afridi <65648689+uzairafridi00@users.noreply.github.com>
2022-12-19 18:33:04 +0500

    Main drawer added

---
+++ Flutter Projects/meal_app/lib/widgets/main_drawer.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import '../screens/filters_screen.dart';
+
+class MainDrawer extends StatelessWidget {
+  const MainDrawer({super.key});
+
+  Widget buildListTile(String title, IconData icon, VoidCallback tapHandler) {
+    return ListTile(
+      onTap: tapHandler,
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        title,
+        style: const TextStyle(
+            fontFamily: 'RobotoCondensed',
+            fontSize: 24,
+            fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          Container(
+            height: 120,
+            width: double.infinity,
+            padding: const EdgeInsets.all(30),
+            alignment: Alignment.centerLeft,
+            color: Theme.of(context).colorScheme.secondary,
+            child: Text(
+              'Cooking Up',
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 30,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          buildListTile(
+            'Meals',
+            Icons.restaurant,
+            // () => Navigator.of(context).pushNamed('/'),
+            () {
+              Navigator.of(context).pushNamed('/');
+            },
+          ),
+          buildListTile(
+            'Settings',
+            Icons.settings,
+            () {
+              Navigator.of(context).pushNamed(FiltersScreen.routeName);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
